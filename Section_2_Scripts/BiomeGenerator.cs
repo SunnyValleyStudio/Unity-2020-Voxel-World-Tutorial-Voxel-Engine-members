b145ac57e7ec28c598e0e1a4ac3de6e7a877c09d
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-06-04 15:06:58 +0200

    Section 2 - Video 5 MutliOctave Perlin Noise

--- Section_2_Scripts/BiomeGenerator.cs
+++ Section_2_Scripts/BiomeGenerator.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class BiomeGenerator : MonoBehaviour
 {
     public int waterThreshold = 50;
-    public float noiseScale = 0.03f;
+
+    public NoiseSettings biomeNoiseSettings;
+
     public ChunkData ProcessChunkColumn(ChunkData data, int x, int z, Vector2Int mapSeedOffset)
     {
-        float noiseValue = Mathf.PerlinNoise((mapSeedOffset.x + data.worldPosition.x + x) * noiseScale, (mapSeedOffset.y + data.worldPosition.z + z) * noiseScale);
-        int groundPosition = Mathf.RoundToInt(noiseValue * data.chunkHeight);
+        biomeNoiseSettings.worldOffset = mapSeedOffset;
+        int groundPosition = GetSurfaceHeightNoise(data.worldPosition.x + x, data.worldPosition.z + z, data.chunkHeight);
+
         for (int y = 0; y < data.chunkHeight; y++)
         {
             BlockType voxelType = BlockType.Dirt;
@@ -38,5 +41,13 @@ public class BiomeGenerator : MonoBehaviour
             Chunk.SetBlock(data, new Vector3Int(x, y, z), voxelType);
         }
         return data;
+    }
+
+    private int GetSurfaceHeightNoise(int x, int z, int chunkHeight)
+    {
+        float terrainHeight = MyNoise.OctavePerlin(x, z, biomeNoiseSettings);
+        terrainHeight = MyNoise.Redistribution(terrainHeight, biomeNoiseSettings);
+        int surfaceHeight = MyNoise.RemapValue01ToInt(terrainHeight, 0, chunkHeight);
+        return surfaceHeight;
     }
 }
