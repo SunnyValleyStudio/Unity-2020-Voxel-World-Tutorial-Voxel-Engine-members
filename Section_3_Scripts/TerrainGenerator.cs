36d92952a27295d47c2198b2d5a6a5c77b8e7ee2
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2021-09-03 11:57:07 +0200

    Section 3 - Video 12 Desert Biome

--- Section_3_Scripts/TerrainGenerator.cs
+++ Section_3_Scripts/TerrainGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
@@ -9,6 +10,16 @@ public class TerrainGenerator : MonoBehaviour
 
     [SerializeField]
     List<Vector3Int> biomeCenters = new List<Vector3Int>();
+    List<float> biomeNoise = new List<float>();
+
+    [SerializeField]
+    private NoiseSettings biomeNoiseSettings;
+
+    public DomainWarping biomeDomainWarping;
+
+    [SerializeField]
+    private List<BiomeData> biomeGeneratorsData = new List<BiomeData>();
+
 
     public ChunkData GenerateChunkData(ChunkData data, Vector2Int mapSeedOffset)
     {
@@ -29,6 +40,19 @@ public class TerrainGenerator : MonoBehaviour
         biomeCenters = new List<Vector3Int>();
         biomeCenters = BiomeCenterFinder.CalculateBiomeCenters(playerPosition, drawRange, mapSize);
 
+        for (int i = 0; i < biomeCenters.Count; i++)
+        {
+            Vector2Int domainWarpingOffset
+                = biomeDomainWarping.GenerateDomainOffsetInt(biomeCenters[i].x, biomeCenters[i].y);
+            biomeCenters[i] += new Vector3Int(domainWarpingOffset.x, 0, domainWarpingOffset.y);
+        }
+        biomeNoise = CalculateBiomeNoise(biomeCenters, mapSeedOffset);
+    }
+
+    private List<float> CalculateBiomeNoise(List<Vector3Int> biomeCenters, Vector2Int mapSeedOffset)
+    {
+        biomeNoiseSettings.worldOffset = mapSeedOffset;
+        return biomeCenters.Select(center => MyNoise.OctavePerlin(center.x, center.y, biomeNoiseSettings)).ToList();
     }
 
     private void OnDrawGizmos()
@@ -40,4 +64,12 @@ public class TerrainGenerator : MonoBehaviour
             Gizmos.DrawLine(biomCenterPoint, biomCenterPoint + Vector3.up * 255);
         }
     }
+}
+
+[Serializable]
+public struct BiomeData
+{
+    [Range(0f, 1f)]
+    public float temperatureStartThreshold, temperatureEndThreshold;
+    public BiomeGenerator biomeTerrainGenerator;
 }
